333d85bf0ee8f42c6fa7eafa0b811727372356d9
Claire <clairelogan16@gmail.com>
2021-06-29 10:26:08 -0700

    commit tree fdf872f789cb3cd8c876032b97fd8aa1769a2f66

--- index.js
+++ index.js
@@ -1,31 +1,58 @@
 
 
-const returnFirstTwoDrivers = function(drivers) {
-    return drivers.slice(0,2);
-}
+// const returnFirstTwoDrivers = function(drivers) {
+//     return drivers.slice(0,2);
+// }
 
-const returnLastTwoDrivers = function(drivers){
-    return drivers.slice(drivers.length-2);
-}
+// const returnLastTwoDrivers = function(drivers){
+//     return drivers.slice(drivers.length-2);
+// }
+
+// const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+// function createFareMultiplier(int) {
+//     return function(fare) {
+//         return int * fare;
+//     }
+// }
+
+// function fareDoubler(fare) {
+//     const doubledFare = createFareMultiplier(2)(fare);
+//     return doubledFare;
+// }
+
+// function fareTripler(fare) {
+//     const tripledFare = createFareMultiplier(3)(fare);
+//     return tripledFare;
+// }
+
+// function selectDifferentDrivers(drivers, driverFunction) {
+//     return driverFunction(drivers);
+// }
+
+
+const returnFirstTwoDrivers = function(drivers){return drivers.slice(0,2);}
+
+const returnLastTwoDrivers =function(drivers){return drivers.slice(-2);}
 
 const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
 
-function createFareMultiplier(int) {
-    return function(fare) {
+function createFareMultiplier(int){
+    return function(fare){
         return int * fare;
     }
 }
 
 function fareDoubler(fare) {
-    const doubledFare = createFareMultiplier(2)(fare);
-    return doubledFare;
+    const doubledFair = createFareMultiplier(2)(fare);
+    return doubledFair;
 }
 
 function fareTripler(fare) {
-    const tripledFare = createFareMultiplier(3)(fare);
-    return tripledFare;
+    const tripledFair = createFareMultiplier(3)(fare);
+    return tripledFair;
 }
 
-function selectDifferentDrivers(drivers, driverFunction) {
+function selectDifferentDrivers(drivers, driverFunction){
     return driverFunction(drivers);
 }
